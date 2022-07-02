e00e74965cc71433efa0beaffec40083a06bd02e
Back2Basic-Tristan <tristandecierdo11@gmail.com>
2022-07-02 10:17:35 +0800

    Files

---
+++ app/Http/Controllers/Admin/MenuController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\MenuStoreRequest;
+use App\Models\Category;
+use App\Models\Menu;
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $menus = Menu::all();
+        return view('admin.menus.index', compact('menus'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        return view('admin.menus.create', compact("categories"));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(MenuStoreRequest $request)
+    {
+        $destination_path = 'public/images/menus/';
+        $file = $request->file('image');
+        $path = $file->storeAs($destination_path,$file->getClientOriginalName());
+
+
+        $menu = Menu::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'image' => $file->getClientOriginalName()
+        ]);
+        //$categories = Category::all();
+        //return view('admin.categories.index', compact('categories'));
+       // return view('admin.categories.index');
+        if ($request->has('categories')){
+            $menu->categories()->attach($request->categories);
+        }
+
+       return $this->index(); 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Menu $menu)
+    {
+        $categories = Category::all();
+        return view('admin.menus.edit', compact('menu', 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
